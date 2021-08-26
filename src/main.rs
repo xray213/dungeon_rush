b832f2f22d41f4afe305627e7e01ac02ab08f192
zero <cminquan@outlook.com>
2021-08-26 23:51:42 +0800

    init bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(WindowDescriptor {
+            title: "dungeon rush".to_string(),
+            width: 800.0,
+            height: 600.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
